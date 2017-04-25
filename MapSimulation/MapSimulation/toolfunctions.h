4e4e3d49d3a0d72532b6f8615145cd3fcd85b46b
Sumin Han <hsm6911@kaist.ac.kr>
2017-04-25 10:32:26 +0900

    20170425 new mapsimulation project

--- MapSimulation/MapSimulation/toolfunctions.h
+++
@@ -1,9 +0,0 @@
-#ifndef TOOL_FUNC_H_
-#define TOOL_FUNC_H_
-
-double dist(double x1, double y1, double x2, double y2);
-void normalize(double &vx, double &vy);
-int vec_equal(double vx, double vy, double ux, double uy);
-bool compare(__int64 i, __int64 j);
-
-#endif
--- OSMcrawling/MapData_Crawling.py
+++ OSMcrawling/MapData_Crawling.py
@@ -8,7 +8,7 @@ from lxml import html
 
 nf= open('MapData.txt','w')              # open text file to write output
 
-with open('kaistmap.osm.xml','r') as f:      # open html file to read data
+with open('kaistmap-refined.osm.xml','r') as f:      # open html file to read data
     page = f.read()
 
 
@@ -20,35 +20,77 @@ ways = soup('way', {})
 print "nodes", len(nodes)
 print "ways", len(ways)
 
-nf.write("# nodes\n")
 node_list = []
-for n in nodes:
-   node_list += [(n['id'], n['lat'], n['lon'])]
-   tmpstr = "v\t" + n['id'] + "\t" + n['lat'] + "\t" + n['lon'] + "\n"
-   nf.write(tmpstr)
+node_reindex = []
+buildings = []
+forests = []
 
-nf.write("# polygons\n")
+idx = 0;
+usedidx = 0;
+for n in nodes:
+   node_list += [(idx, n['id'], n['lat'], n['lon'])]
+   idx += 1
+
+def search(mid):
+   global node_reindex, usedidx
+   for (idx, nid, nlat, nlon) in node_list:
+      if(mid == nid):
+         if len(node_reindex) > 0:
+            for (ridx, pidx, mlat, mlon) in node_reindex:
+               if(idx == pidx):
+                  return ridx
+         node_reindex += [(usedidx, idx, nlat, nlon)];
+         usedidx += 1
+         return usedidx-1
+   return -1
+
+
+
 bnum = 0
+fnum = 0
 for w in ways:
    isBuild = False
+   isForest = False
    bname = '_'
    for t in w.select("tag"):
       k = t['k']
+      v = t['v']
       if k == 'building':
          isBuild = True
          bnum += 1
-      if k == 'name':
-         bname = t['v']
-
+      if k == 'natural' and v == 'wood':
+         fnum += 1
+         isForest = True
+      
    if isBuild:
-      tmpstr = "p " + bname.encode('utf8')
+      if not w.find("nd"): continue
+      bname = t['v']
+      tmpstr = "b " + bname.encode('utf8')
       for nd in w.select("nd"):
-         kk = nd['ref']
-         tmpstr = tmpstr + "\t" + kk;
-         
+         kk = search(nd['ref'])
+         tmpstr = tmpstr + "\t" + str(kk);   
       tmpstr = tmpstr + "\n"
-      nf.write(tmpstr)
+      buildings += [tmpstr]
 
-print "There are ", bnum, "buildings"
+   if isForest:
+      if not w.find("nd"): continue
+      tmpstr = "f " + bname.encode('utf8')
+      for nd in w.select("nd"):
+         kk = search(nd['ref'])
+         tmpstr = tmpstr + "\t" + str(kk);   
+      tmpstr = tmpstr + "\n"
+      forests += [tmpstr]
+      
 
+print "There are ", len(node_reindex), "/", len(node_list), " ", bnum, "buildings and ", fnum, "forests."
+nf.write("i\t" + str(len(node_reindex)) + "\t" + str(len(buildings)) + "\t" + str(len(forests)) + "\n")
+nf.write("# nodes\n")
+for (ridx, idx, mlat, mlon) in node_reindex:
+   nf.write("n\t" + str(mlat) + "\t" + str(mlon) + "\n")
+nf.write("# buildings\n")
+for btext in buildings:
+   nf.write(btext)
+nf.write("# forests\n")
+for ftext in forests:
+   nf.write(ftext)
 nf.close()
