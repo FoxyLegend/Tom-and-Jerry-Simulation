250e14eda31fe89a23dc5215c19f42dcb8e1347c
Sumin Han <hsm6911@kaist.ac.kr>
2017-04-30 11:26:53 +0900

    20170430 signal 64bit int world success

--- MapCalculation/MapCalculation/toolfunctions.cpp
+++ MapCalculation/MapCalculation/toolfunctions.cpp
@@ -15,6 +15,22 @@ double dist(double x1, double y1, double x2, double y2) {
 	return sqrt(dx*dx + dy*dy);
 }
 
+my_t dist(my_t x1, my_t y1, my_t x2, my_t y2) {
+	my_t dx = x2 - x1;
+	my_t dy = y2 - y1;
+	if (dx == 0) {
+		return abs(dy);
+	}
+	if (dy == 0) {
+		return abs(dx);
+	}
+	return (my_t)sqrt(dx*dx + dy*dy);
+}
+
+double veclen(my_t vx, my_t vy) {
+	return (my_t)sqrt(vx*vx + vy*vy);
+}
+
 void normalize(double &vx, double &vy) {
 	double len = sqrt(vx*vx + vy*vy);
 	vx /= len;
